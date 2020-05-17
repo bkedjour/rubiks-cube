f864199c5291f77731e8ac12f3e5e8e6fc1ae2f8
Badreddine Kedjour <b.kedjour@outlook.com>
2020-05-17 18:04:55 +0200

    added cell 3d representation

--- src/engine/RubiksCube/RubiksCube.Engine/ICube.cs
+++ src/engine/RubiksCube/RubiksCube.Engine/ICube.cs
@@ -1,7 +1,13 @@
-﻿namespace RubiksCube.Engine
+﻿using System.Collections.Generic;
+
+namespace RubiksCube.Engine
 {
     public interface ICube
     {
+        IEnumerable<Face> Faces { get; }
+
+        Status Status { get; }
+
         void Shuffle();
 
         void Rotate(Axis axis, int angle);
--- src/ui/RubiksCube.Ui/RubiksCubeApplication.cs
+++ src/ui/RubiksCube.Ui/RubiksCubeApplication.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Numerics;
+using RubiksCube.Engine;
 using RubiksCube.Ui.Base;
 using Veldrid;
 using Veldrid.SPIRV;
@@ -9,144 +11,114 @@ namespace RubiksCube.Ui
     public class RubiksCubeApplication : ApplicationBase
     {
         private static CommandList _commandList;
-        private static DeviceBuffer _vertexBuffer;
-        private static DeviceBuffer _indexBuffer;
-        
         private static Shader[] _shaders;
-        private static Pipeline _pipeline;
 
-        private DeviceBuffer _projViewWorldBuffer;
-        private ResourceSet _projViewWorldSet;
+        private readonly ICube _cube;
 
-        private VertexPositionColor[] _quadVertices;
-        private ushort[] _quadIndices;
-
-        private float _ticks;
+        private List<CellInfo> _cells;
 
         public RubiksCubeApplication(IWindow window) : base(window)
         {
-            Camera.Position = new Vector3(0, 0, 1f);
+            Camera.Position = new Vector3(0, 0, 5f);
+            
+            var cubeFactory = new CubeFactory();
+            _cube = cubeFactory.CreateCube();
         }
 
         protected override void CreateResources(ResourceFactory factory)
-        {
-            _vertexBuffer = factory.CreateBuffer(new BufferDescription(4 * VertexPositionColor.SizeInBytes, BufferUsage.VertexBuffer));
-            _indexBuffer = factory.CreateBuffer(new BufferDescription(4 * sizeof(ushort), BufferUsage.IndexBuffer));
-            _projViewWorldBuffer = factory.CreateBuffer(new BufferDescription(3 * 64, BufferUsage.UniformBuffer));
-
-            CreateRectangle();
-
-            GraphicsDevice.UpdateBuffer(_vertexBuffer, 0, _quadVertices);
-            GraphicsDevice.UpdateBuffer(_indexBuffer, 0, _quadIndices);
-
-            var projViewWorldLayout = factory.CreateResourceLayout(
-                new ResourceLayoutDescription(
-                    new ResourceLayoutElementDescription("ProjectionViewWorldBuffer", ResourceKind.UniformBuffer, ShaderStages.Vertex)));
-
-
-            var pipelineDescription = new GraphicsPipelineDescription
-            {
-                BlendState = BlendStateDescription.SingleOverrideBlend,
-                DepthStencilState = DepthStencilStateDescription.DepthOnlyLessEqual,
-                RasterizerState = RasterizerStateDescription.Default,
-                PrimitiveTopology = PrimitiveTopology.TriangleStrip,
-                ResourceLayouts = new []{projViewWorldLayout},
-                ShaderSet = LoadShaders(factory),
-                Outputs = GraphicsDevice.SwapchainFramebuffer.OutputDescription
-            };
-
-            _pipeline = factory.CreateGraphicsPipeline(pipelineDescription);
-
-            _projViewWorldSet = factory.CreateResourceSet(new ResourceSetDescription(projViewWorldLayout,_projViewWorldBuffer));
-            
-            _commandList = factory.CreateCommandList();
-        }
-
-        private void CreateRectangle()
-        {
-            _quadVertices = new[]
-            {
-                new VertexPositionColor(new Vector3(-.75f, .75f, -1f), RgbaFloat.Red),
-                new VertexPositionColor(new Vector3(.75f, .75f, -1f), RgbaFloat.Green),
-                new VertexPositionColor(new Vector3(-.75f, -.75f, -1f), RgbaFloat.Blue),
-                new VertexPositionColor(new Vector3(.75f, -.75f, -1f), RgbaFloat.Yellow)
-            };
-
-            _quadIndices = new ushort[] {0, 1, 2, 3};
-        }
-
-        private ShaderSetDescription LoadShaders(ResourceFactory factory)
         {
             _shaders = factory.CreateFromSpirv(
                 new ShaderDescription(ShaderStages.Vertex, ReadEmbeddedAssetBytes("Vertex.glsl"), "main"),
                 new ShaderDescription(ShaderStages.Fragment, ReadEmbeddedAssetBytes("Fragment.glsl"), "main"));
 
-            return new ShaderSetDescription(
-                new[]
-                {
-                    new VertexLayoutDescription(
-                        new VertexElementDescription("Position", VertexElementSemantic.TextureCoordinate,
-                            VertexElementFormat.Float3),
-                        new VertexElementDescription("Color", VertexElementSemantic.TextureCoordinate,
-                            VertexElementFormat.Float4))
-                }, _shaders);
+            _commandList = factory.CreateCommandList();
+
+            InitializeCube(factory);
         }
 
-
-        protected override void Draw(float deltaSeconds)
+        private void InitializeCube(ResourceFactory factory)
         {
-            _ticks += deltaSeconds;
+            _cells = new List<CellInfo>();
 
-            UpdateUniformBuffers();
-
-            _commandList.Begin();
-
-            
-
-            _commandList.SetFramebuffer(GraphicsDevice.SwapchainFramebuffer);
-            _commandList.ClearColorTarget(0, RgbaFloat.Black);
-            _commandList.ClearDepthStencil(1f);
-            _commandList.SetPipeline(_pipeline);
-
-            _commandList.SetVertexBuffer(0, _vertexBuffer);
-            _commandList.SetIndexBuffer(_indexBuffer, IndexFormat.UInt16);
-            _commandList.SetGraphicsResourceSet(0, _projViewWorldSet);
-
-            _commandList.DrawIndexed(4, 1, 0, 0, 0);
-
-            _commandList.End();
-            
-            GraphicsDevice.SubmitCommands(_commandList);
-            GraphicsDevice.SwapBuffers();
-        }
-
-        private void UpdateUniformBuffers()
-        {
-            var projViewWorldInfo = new ProjViewWorldInfo
+            foreach (var face in _cube.Faces)
             {
-                Projection = Matrix4x4.CreatePerspectiveFieldOfView(1.0f, (float)Window.Width / Window.Height, 0.5f, 100f),
-                View = Matrix4x4.CreateLookAt(Camera.Position, Camera.Position + Camera.Forward, Vector3.UnitY),
-                World = Matrix4x4.Identity
-            };
-            
-            _commandList.UpdateBuffer(_projViewWorldBuffer, 0, ref projViewWorldInfo);
+                for (int row = 0; row < face.Cells.GetLength(0); row++)
+                {
+                    for (int col = 0; col < face.Cells.GetLength(1); col++)
+                    {
+                        _cells.Add(new CellInfo(face.Cells[row, col], new Vector3((row - 1) * 1.05f, (col - 1) * -1.05f, 1.1f), factory, GraphicsDevice,
+                            _commandList, _shaders)
+                        {
+                            Rotation = GetRotation(face.Side)
+                        });
+                    }
+                }
+                
+            }
         }
 
-        public static float DegreesToRadians(float degrees)
+        private Matrix4x4 GetRotation(Side side)
+        {
+            switch (side)
+            {
+                case Side.Front:
+                    return Matrix4x4.Identity;
+                case Side.Back:
+                    return Matrix4x4.CreateRotationY(DegreesToRadians(180));
+                case Side.Right:
+                    return Matrix4x4.CreateRotationY(DegreesToRadians(90));
+                case Side.Left:
+                    return Matrix4x4.CreateRotationY(DegreesToRadians(-90));
+                case Side.Up:
+                    return Matrix4x4.CreateRotationX(DegreesToRadians(-90));
+                case Side.Down:
+                    return Matrix4x4.CreateRotationX(DegreesToRadians(90));
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(side), side, null);
+            }
+        }
+
+        private float DegreesToRadians(float degrees)
         {
             return degrees * (float)Math.PI / 180f;
         }
 
+        protected override void Draw(float deltaSeconds)
+        {
+            var projection =
+                Matrix4x4.CreatePerspectiveFieldOfView(1.0f, (float)Window.Width / Window.Height, 0.5f, 100f);
+
+            var view = Matrix4x4.CreateLookAt(Camera.Position, Camera.Position + Camera.Forward, Vector3.UnitY);
+
+            foreach (var cell in _cells)
+            {
+                cell.Rotation *= Matrix4x4.CreateRotationY(deltaSeconds) * Matrix4x4.CreateRotationX(deltaSeconds/2) * Matrix4x4.CreateRotationZ(deltaSeconds/4);
+                cell.Update(deltaSeconds, projection, view);
+            }
+
+            _commandList.Begin();
+            _commandList.SetFramebuffer(GraphicsDevice.SwapchainFramebuffer);
+            _commandList.ClearColorTarget(0, RgbaFloat.Black);
+            _commandList.ClearDepthStencil(1f);
+
+            foreach (var cell in _cells)
+                cell.Draw(deltaSeconds);
+
+            _commandList.End();
+
+            GraphicsDevice.SubmitCommands(_commandList);
+            GraphicsDevice.SwapBuffers();
+        }
+
         protected override void OnDeviceDestroyed()
         {
-            _pipeline.Dispose();
-
             foreach (var shader in _shaders)
                 shader.Dispose();
 
             _commandList.Dispose();
-            _vertexBuffer.Dispose();
-            _indexBuffer.Dispose();
+
+            foreach (var cell in _cells)
+                cell.Dispose();
 
             base.OnDeviceDestroyed();
         }
